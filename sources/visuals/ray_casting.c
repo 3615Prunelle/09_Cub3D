768ca03648901d6003bbb01e749d6ba4b43e3628
lehMaxi <lehmann.max95@gmail.com>
2026-03-29 14:32:34 +0200

    Fix wall side determination logic in ray casting

--- sources/visuals/ray_casting.c
+++ sources/visuals/ray_casting.c
@@ -12,11 +12,38 @@
 
 #include "cub3d.h"
 
+void	set_wallside(t_ray *ray, int[2] position, int[2] wall_position)
+{
+	if (wall_position[0] > position[0])
+		ray->wall[0] = 'E';
+	if (wall_position[0] < position[0])
+		ray->wall[0] = 'W';
+	if (wall_position[1] > position[1])
+		ray->wall[0] = 'N';
+	if (wall_position[1] > position[1])
+		ray->wall[0] = 'S';
+}
+
 void	calculate_ray_length_and_wallside(t_cube *game, t_ray *ray, float deg, float[2] position)
 {
 	float	wall_posi[2];
 	int		int_pos[2];
 	int		int_wall_p[2];
+
+	wall_posi[0] = position[0] * 32;
+	wall_posi[1] = position[1] * 32;
+	coordinates_float_to_int(int_wall_p, wall_posi);
+	coordinates_float_to_int(int_pos, position);
+	while (int_wall_p[0] / 32 == int_pos[0] && int_wall_p[1] / 32 == int_pos[1])
+	{
+		wall_posi[0] -= ray->step_x;
+		wall_posi[1] -= ray->step_y;
+		coordinates_float_to_int(int_wall_p, wall_posi);
+	}
+	int_wall_p[0] /=32;
+	int_wall_p[1] /=32;
+	set_wallside(ray, int_pos, int_wall_p);
+	ray->length = fabs(fabs(wall_posi[1] - game->player->position[1]) / sin(deg * DEG_TO_RAD));
 }
 
 void	cast_horizontaly(t_cube *game, t_ray *ray, float deg, char **map)
